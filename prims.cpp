6e29bb6c822fe44cfdd6b2bf48ae833a6dd66568
arpitgpta <arpit.cs18@nitp.ac.in>
2020-01-22 01:04:42 +0530

    mst formed

--- prims.cpp
+++ prims.cpp
@@ -1,7 +1,15 @@
 // g++ prims.cpp -o prims && ./prims
 
 /*
-
+    algo:
+        1. first we are choosing starting virtex for mst
+        2. then we are inserting all the adgesent edges of starting node in a min heap
+        3. now take away the edge which is on the top of the heap
+        4. if it is not mking cycle 
+            4.1 store the edge
+            4.2 mark new virtex as selected
+            4.3 insert all adgesent edges of new virtex in the heap
+        5. repeat step 3 and 4 while selected edges are less than 8
 
 */
 
@@ -22,6 +30,30 @@ int dustbin;
 
 vector<vin> heap(100, vin(3, INT_MAX));
 int heap_size = 1;
+int n = 9;    
+vector<vin> adj_matrix(n+1, vin(n+1, INT_MAX));
+vector<vin> selected_edges;
+map<int, bool> selected;
+
+void pre_processing(){
+    adj_matrix[1][2] = 2;
+    adj_matrix[2][3] = 4;
+    adj_matrix[3][8] = 1;
+    adj_matrix[3][4] = 3;
+    adj_matrix[1][4] = 3;
+    adj_matrix[4][5] = 3;
+    adj_matrix[4][7] = 2;
+    adj_matrix[5][6] = 4;
+    adj_matrix[6][7] = 5;
+    adj_matrix[7][8] = 2;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if (adj_matrix[j][i] != INT_MAX) adj_matrix[i][j] = adj_matrix[j][i];
+        }
+    }
+}
+
 
 void insert(vin edge){
     
@@ -61,62 +93,46 @@ vin _delete(){
 }
 
 int main(){
+    pre_processing();
 
-    int n = 9;
-    
-    vector<vin> matrix(n+1, vin(n+1, INT_MAX));
-    vector<vin> selected_edges;
-    map<int, bool> selected;
-
-    matrix[1][2] = 2;
-    matrix[2][3] = 4;
-    matrix[3][8] = 1;
-    matrix[3][4] = 3;
-    matrix[1][4] = 3;
-    matrix[4][5] = 3;
-    matrix[4][7] = 2;
-    matrix[5][6] = 4;
-    matrix[6][7] = 5;
-    matrix[7][8] = 2;
-
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            if (matrix[j][i] != INT_MAX) matrix[i][j] = matrix[j][i];
-        }
-    }
-
+    // 1.
     int start;
     cout << "Enter starting virtex" << endl;
     cin >> start;
  
     selected[start] = true;
  
+    //  2.
     for (int i = 1; i <= n; i++){
-        if (matrix[start][i] != INT_MAX){
-            vin temp = {start, i, matrix[start][i]};
+        if (adj_matrix[start][i] != INT_MAX){
+            vin temp = {start, i, adj_matrix[start][i]};
             insert(temp);
         }
     }
     
     int taken = 0;
     vector<vin> answer;
+    
+    //  5.
     while (taken < 7){
+        //  3.
         vin temp(3);
         temp = _delete();
 
         if (!( selected[temp[0]] == true && selected[temp[1]] == true )){
+            // 4. 
             answer.pb(temp);
             taken++;
             int new_vtx = !selected[temp[0]] ? temp[0] : temp[1];
             selected[new_vtx] = true;
             for (int i = 1; i <= n; i++){
-                if (matrix[new_vtx][i] != INT_MAX){
-                    vin temp = {new_vtx, i, matrix[new_vtx][i]};
+                if (adj_matrix[new_vtx][i] != INT_MAX){
+                    vin temp = {new_vtx, i, adj_matrix[new_vtx][i]};
                     insert(temp);
                 }       
             }
         }
     }
-
+    // printing answer
     for(auto k : answer) cout << k[0] << " " << k[1] << " " << k[2] << endl;
 }
